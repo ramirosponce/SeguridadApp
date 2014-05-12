f33ecb5b7b8458629b6c32bfca7efa47a3c0b646
juan felippo <juan@simpleapp.com.ar>
2014-05-12 15:31:23 -0300

    aboutScreen

---
+++ SeguridadApp/SeguridadApp/Controllers/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  SeguridadApp
+//
+//  Created by juan felippo on 12/05/14.
+//  Copyright (c) 2014 Ramiro Ponce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+{
+__weak IBOutlet UITextView* termsofuseTextView;
+}
+@end
